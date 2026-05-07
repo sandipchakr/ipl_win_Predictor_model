fc461c4379b5b41353c620f510cee254690b1e24
sandip chakraborty <unicornsc77@gmail.com>
2026-05-07 21:27:16 +0530

    add all files

---
+++ app.py
@@ -0,0 +1,194 @@
+import pickle
+import numpy as np
+import pandas as pd
+import streamlit as st
+
+# load the dataset:-
+df = pd.read_csv("ipl_dataset.csv",encoding="latin1",low_memory=False)
+
+# load model:-
+with open("ipl_model.pkl", "rb") as f:
+    data = pickle.load(f)
+
+model = data["model"]
+encoders = data["encoders"]
+# print(encoders)
+
+def predict_winner(team_a, team_b, venue, city, toss_decision,
+                   team_a_toss_win, team_a_NRR, team_b_NRR, team_a_last5_win_pct, 
+                   team_b_last5_win_pct, h2h_win_pct, team_a_home, team_b_home):
+    
+    # input_data = np.array([[team_a, team_b, venue, city, toss_decision, team_a_toss_win, 
+    #                         team_a_NRR, team_b_NRR, team_a_last5_win_pct, team_b_last5_win_pct, 
+    #                         h2h_win_pct, team_a_home, team_b_home]])
+
+    input_data = pd.DataFrame([{
+    "team_a": team_a,
+    "team_b": team_b,
+    "venue": venue,
+    "city": city,
+    "toss_decision": toss_decision,
+    "team_a_toss_win": team_a_toss_win,
+    "team_a_NRR": team_a_NRR,
+    "team_b_NRR": team_b_NRR,
+    "team_a_last5_win_pct": team_a_last5_win_pct,
+    "team_b_last5_win_pct": team_b_last5_win_pct,
+    "h2h_win_pct": h2h_win_pct,
+    "team_a_home": team_a_home,
+    "team_b_home": team_b_home
+}])
+    
+    # input_data[:, 0] = encoders["team_a"].transform(input_data[:, 0])
+    # input_data[:, 1] = encoders["team_b"].transform(input_data[:, 1])
+    # input_data[:, 2] = encoders["venue"].transform(input_data[:, 2])
+    # input_data[:, 3] = encoders["city"].transform(input_data[:, 3])
+    # input_data[:, 4] = encoders["toss_decision"].transform(input_data[:, 4])
+
+    input_data["team_a"] = encoders["team_a"].transform(input_data["team_a"])
+    input_data["team_b"] = encoders["team_b"].transform(input_data["team_b"])
+    input_data["venue"] = encoders["venue"].transform(input_data["venue"])
+    input_data["city"] = encoders["city"].transform(input_data["city"])
+    input_data["toss_decision"] = encoders["toss_decision"].transform(input_data["toss_decision"])
+
+    prediction = model.predict(input_data)
+    return prediction[0]
+
+# print(predict_winner(
+#     "Lucknow Super Giants",
+#     "Royal Challengers Bengaluru",
+#     "Eden Gardens",
+#     "Lucknow",
+#     "Field",
+#     0,-1.07,
+#     1.42,
+#     0.70,
+#     0.20,
+#     0.50,
+#     1,
+#     0
+# ))
+
+# create UI:-
+
+
+st.markdown(
+    "<h1 style="">🏏 IPL Winner Predictor 🤖</h1>",
+    unsafe_allow_html=True
+)
+def calculate_h2h(team_a, team_b):
+
+    matches = df[
+        (
+            (df["team_a"] == team_a) &
+            (df["team_b"] == team_b)
+        ) |
+        (
+            (df["team_a"] == team_b) &
+            (df["team_b"] == team_a)
+        )
+    ]
+
+    if len(matches) == 0:
+        return 0.5
+
+    team_a_wins = (matches["winner"] == team_a).sum()
+
+    return round((team_a_wins / len(matches)*100), 2)
+
+st.write("## input data")
+col1,col2 = st.columns(2)
+team_a = col1.selectbox(
+    "Select Team_A",
+    df["team_a"].dropna().unique()
+)
+team_b = col2.selectbox(
+    "Select Team_B",
+    df["team_b"].dropna().unique()
+)
+if team_a == team_b:
+    st.error("Both teams cannot be same")
+    st.stop()
+
+h2h_win_pct = calculate_h2h(team_a, team_b)
+st.write(f"Head-to-Head Win % for {team_a}: {h2h_win_pct}")
+venue = st.selectbox(
+    "Select Venue",
+    df["venue"].dropna().unique()
+)
+
+city = st.selectbox(
+    "Select City",
+    df["city"].dropna().unique()
+)
+col1,col2 = st.columns(2)
+team_a_toss_win = col1.radio(
+    "Did Team A Win Toss?",
+    ["Yes", "No"]
+)
+team_a_toss_win = 1 if team_a_toss_win == "Yes" else 0
+
+toss_decision = col2.selectbox(
+    "Toss Decision",
+    df["toss_decision"].dropna().unique()
+)
+team_a_last5_wins = st.slider(
+    f"{team_a} Wins in Last 5 Matches",
+    0,
+    5,
+    3
+)
+
+team_b_last5_wins = st.slider(
+    f"{team_b} Wins in Last 5 Matches",
+    0,
+    5,
+    2
+)
+st.subheader("Net Run Rate")
+
+col1, col2 = st.columns(2)
+
+team_a_NRR = col1.slider(
+    f"{team_a} NRR",
+    min_value=-5.0,
+    max_value=5.0,
+    value=0.0,
+    step=0.01
+)
+
+team_b_NRR = col2.slider(
+    f"{team_b} NRR",
+    min_value=-5.0,
+    max_value=5.0,
+    value=0.0,
+    step=0.01
+)
+
+col1,col2 = st.columns(2)
+team_a_home = col1.radio(
+    f"{team_a} home ground ...?",
+    ["Yes", "No"]
+)
+team_b_home = col2.radio(
+    f"{team_b} home ground ...?",
+    ["Yes", "No"]
+)
+
+if st.button("Predict Winner"):
+    prediction = predict_winner(
+        team_a,
+        team_b,
+        venue,
+        city,
+        toss_decision,
+        team_a_toss_win,
+        team_a_NRR,
+        team_b_NRR,
+        team_a_last5_wins/5, # convert to win percentage
+        team_b_last5_wins/5, # convert to win percentage
+        h2h_win_pct/100, # convert to decimal
+        1 if team_a_home == "Yes" else 0,
+        1 if team_b_home == "Yes" else 0
+    )
+    winner = team_a if prediction == 1 else team_b
+    st.success(f"Predicted Winner: {winner}")
---
+++ train_version_2.py
@@ -0,0 +1,135 @@
+import pandas as pd
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score,precision_score,recall_score
+
+import pickle
+
+#load dataset:-
+df = pd.read_csv("ipl_dataset.csv",encoding="latin1",low_memory=False)
+df = df.dropna(subset=["winner", "team_a", "team_b"])
+
+df["team_a_won"] = (df["winner"] == df["team_a"]).astype(int)    # team_a_won is the output
+
+# head to head feature:- win rate of team_a vs team_b
+
+h2h = (
+    df.groupby(["team_a","team_b"])["team_a_won"]
+    .expanding().mean().shift(1).reset_index(level=[0,1],drop=True)
+)
+df["h2h_win_pct"] = h2h.fillna(0.5)
+
+# home ground advantage
+# print(df["city"])
+HOME_CITIES = {
+    "Mumbai Indians": "Mumbai",
+    "Chennai Super Kings": "Chennai",
+    "Royal Challengers Bangalore": "Bengaluru",
+    "Kolkata Knight Riders": "Kolkata",
+    "Delhi Capitals": "Delhi",
+    "Rajasthan Royals": "Jaipur",
+    "Punjab Kings": "Mohali",
+    "Sunrisers Hyderabad": "Hyderabad",
+}
+
+df["team_a_home"] = df.apply(
+    lambda r: 1 if HOME_CITIES.get(r["team_a"])==r["city"] else 0, axis=1
+)
+df["team_b_home"] = df.apply(
+    lambda r: 1 if HOME_CITIES.get(r["team_b"])==r["city"] else 0, axis=1
+)
+df["team_a_home"] = df["team_a_home"].fillna(0)
+df["team_b_home"] = df["team_b_home"].fillna(0)
+
+# print(df[["team_a","team_b","city","team_a_home","team_b_home"]].tail(5))
+
+# toss advantage feature:-
+df["team_a_toss_win"] = (df["toss_winner"]==df["team_a"]).astype(int)
+
+# encoding______________________________________
+dataset = {}
+# labelencode:-
+dataset_columns = [
+    "team_a",
+    "team_b",
+    "venue",
+    "city",
+    # "toss_winner",
+    "toss_decision",
+    # "winner"
+]
+
+encoders = {}
+for col in dataset_columns:
+    le = LabelEncoder()
+    df[col] = le.fit_transform(df[col])
+    encoders[col] = le
+
+print(encoders)
+# final_dataset = pd.DataFrame(dataset)
+# # print(final_dataset.head(5))
+
+# example feature:-
+x = df[[ 
+    "team_a",
+    "team_b",
+    "venue",
+    "city",
+    # "toss_winner",
+    "toss_decision",
+    "team_a_toss_win",
+    "team_a_NRR",
+    "team_b_NRR",
+    "team_a_last5_win_pct",
+    "team_b_last5_win_pct",
+    "h2h_win_pct", # new add
+    "team_a_home", # new add
+    "team_b_home" # new add
+    ]]
+y = df["team_a_won"] # binary terget
+
+# train test split:-
+x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.2,random_state=42,stratify=y)
+
+# model selection:-
+
+model = RandomForestClassifier( n_estimators=300,
+    random_state=42)
+
+# # train model:-
+
+model.fit(x_train,y_train)
+
+# # predict :-
+
+value = model.predict(x_test)
+print(value)
+
+team_a_names = encoders["team_a"].inverse_transform(x_test["team_a"])
+team_b_names = encoders["team_b"].inverse_transform(x_test["team_b"])
+
+for i in range(len(value)):
+    if value[i] == 1:
+        print(team_a_names[i])
+    else:
+        print(team_b_names[i])
+
+# decode_winner = encoders["winner"].inverse_transform(value)
+# print(decode_winner[0])
+# team_data = x_test[["team_a","team_b"]]
+# plt.plot(team_data,y_test,color="red",marker="^")
+# plt.plot(team_data,value,color="blue",marker="o")
+# plt.show()
+
+print(accuracy_score(y_test,value)*100)
+print(round(precision_score(y_test,value,average="weighted")*100, 2), "%")
+print(round(recall_score(y_test,value,average="weighted")*100, 2), "%")
+
+# save model:-
+with open("ipl_model.pkl","wb") as f:
+    pickle.dump({
+        "model": model,
+        "encoders": encoders
+        },f)
